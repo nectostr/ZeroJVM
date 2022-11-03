73cff8dc26a5f4d56db8553ef3240a0a2b570b07
nectostr <nectostr@euca-10-1-5-154.eucalyptus.internal>
2022-11-03 00:17:13 +0000

    structures templates

---
+++ src/zero_jvm/structures.h
@@ -0,0 +1,23 @@
+#define STATICS_TABLE_SIZE 64
+#define MAX_NAME_SIZE      32
+#define MAX_TYPE_SIZE       4
+#define STATICS_MAP_SIZE   64
+
+// 1byte array, so +offset would work nicely
+char statics_table[STATICS_TABLE_SIZE];
+
+// alternative: offset always /4
+// uint32_t statics_table[STATICS_TABLE_SIZE/4];
+
+struct MapEntry{
+    char name[MAX_NAME_SIZE];
+    char type[MAX_TYPE_SIZE];
+    uint32_t offset;
+};
+
+// Renaming, to simplify usage
+typedef struct MapEntry MapEntry;
+
+
+extern MapEntry statics_map[STATICS_MAP_SIZE];
+
