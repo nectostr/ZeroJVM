3f387a0b4372cfff392d147bf878eeb0eef45230
nectostr <bastinda96@gmail.com>
2022-12-04 07:32:33 +0000

    math exm

--- tests/Entrypoint_math.java
+++ tests/Entrypoint_math.java
@@ -7,17 +7,18 @@ class Entrypoint {
     public static void delay(int duration_ms) {}
     
     public static void main(String argv[]) {
-        int a = 4232;
-        int b = 123;
-        int c = a / b + 345 - 1231 * a;
+        // int a = 4232;
+        // int b = 123;
+        // int c = a / b + 345 - 1231 * a;
 
         
 
-        Entrypoint.println("Hello, world!");
-        Entrypoint.delay(3000);
-        Entrypoint.println(c);
-        Entrypoint.delay(500);
-        Entrypoint.println(2567*0 + 0 - 9);
+        // Entrypoint.println("Hello, world!");
+        // Entrypoint.delay(3000);
+        // Entrypoint.println(c);
+        // Entrypoint.delay(500);
+        int a = -5;
+        Entrypoint.println(a);
         Entrypoint.delay(5000);
     }
 }
