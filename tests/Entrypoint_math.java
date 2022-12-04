c4a10902e2b2e8568cb75f62d762b7d13956f8f8
nectostr <bastinda96@gmail.com>
2022-12-04 00:10:49 +0000

    java clss

--- tests/Entrypoint_math.java
+++ tests/Entrypoint_math.java
@@ -1,15 +1,21 @@
 class Entrypoint {
+
+    public static void println(String s) {}
+    public static void beep(float frequency, int duration) {}
+    public static void delay(int duration_ms) {}
     
     public static void main(String argv[]) {
         int a = 4232;
         int b = 123;
         int c = a / b + 345 - 1231 * a;
 
+        
+
         Entrypoint.println("Hello, world!");
         Entrypoint.delay(3000);
-        Entrypoint.println("" + c);
+        Entrypoint.println(c);
         Entrypoint.delay(5000);
-        Entrypoint.println("" + (2567*0 + 0 - 9));
+        Entrypoint.println(2567*0 + 0 - 9);
         Entrypoint.delay(5000);
     }
 }
